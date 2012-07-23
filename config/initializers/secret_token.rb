9956ad7d9402696016ba29e65dcee4b6e237c3ec
Azamat Mukhamejanov <mukhamejanov.aza@gmail.com>
2012-07-23 16:19:53 +0600

    initialize new rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AcmKbtu::Application.config.secret_token = 'fefbb8f13d3b4029df777124c0f8c9b11d04cb924badc772438e6adee981967c2812df8bddbe64314535427c5aec75f1c7ee4176f1f2e829d8f0a2afab62aaf9'
